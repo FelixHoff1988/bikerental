d9059f3f66128e6328d3b78e48a6e3b39f165e55
eike.wolff <eike.wolff@uni-muenster.de>
2024-02-22 15:55:32 +0100

    :memo: Füge fehlende JavaDocs hinzu

--- src/main/java/de/wwu/sopra/application/App.java
+++ src/main/java/de/wwu/sopra/application/App.java
@@ -1,6 +1,18 @@
 package de.wwu.sopra.application;
 
+/**
+ * Einstiegspunkt in die Anwendung
+ */
 public class App {
+  /**
+   * Standardkonstruktor
+   */
+  public App() {}
+
+  /**
+   * Einstiegspunkt der Anwendung
+   * @param args Argumente zum Programmstart
+   */
   public static void main(String [] args) {
   }
 }
