00adf6e55838ae6d30daa31dfad0d5a3a009cef4
David Rüschhoff <drueschh@uni-muenster.de>
2024-02-22 16:38:42 +0100

    Reservation erstellt

---
+++ src/main/java/entity/reservation/Reservation.java
@@ -0,0 +1,58 @@
+package entity.reservation;
+
+public class Reservation {
+	
+	//Create parameters
+	protected String firstName;
+	protected String lastName;
+	protected String address;
+	protected String email;
+	protected String iban;
+	protected String bic;
+	protected String passwordhash;
+	
+	//Getter, Setter
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getIban() {
+		return iban;
+	}
+	public void setIban(String iban) {
+		this.iban = iban;
+	}
+	public String getBic() {
+		return bic;
+	}
+	public void setBic(String bic) {
+		this.bic = bic;
+	}
+	public String getPasswordhash() {
+		return passwordhash;
+	}
+	public void setPasswordhash(String passwordhash) {
+		this.passwordhash = passwordhash;
+	}
+	
+}
