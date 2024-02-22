f12929b68134de131718a0c262e48499b84ce483
eike.wolff <eike.wolff@uni-muenster.de>
2024-02-22 16:10:52 +0100

    :sparkles: Füge Test für Coverage hinzu

---
+++ src/test/java/de/wwu/sopra/model/application/AppTest.java
@@ -0,0 +1,11 @@
+package de.wwu.sopra.model.application;
+
+import de.wwu.sopra.application.App;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    public void TestMain() {
+        App.main(new String[]{});
+    }
+}
