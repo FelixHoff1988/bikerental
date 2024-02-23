442115a5c09f204b4d098492be14e22cada826d2
Felix Hoff <f_hoff11@uni-muenster.de>
2024-02-23 09:33:21 +0100

    implenmented first window

--- src/main/java/de/wwu/sopra/application/App.java
+++ src/main/java/de/wwu/sopra/application/App.java
@@ -1,18 +1,38 @@
 package de.wwu.sopra.application;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
 /**
  * Einstiegspunkt in die Anwendung
  */
-public class App {
-  /**
-   * Standardkonstruktor
-   */
-  public App() {}
+public class App extends Application {
+	/**
+	 * Standardkonstruktor
+	 */
+	public App() {
+	}
 
-  /**
-   * Einstiegspunkt der Anwendung
-   * @param args Argumente zum Programmstart
-   */
-  public static void main(String [] args) {
-  }
+	/**
+	 * Einstiegspunkt der Anwendung
+	 * 
+	 * @param args Argumente zum Programmstart
+	 */
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Leihrad-\u00dcbersicht");
+
+		GridPane gridPane = new GridPane();
+
+		Scene scene = new Scene(gridPane);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+	}
 }
