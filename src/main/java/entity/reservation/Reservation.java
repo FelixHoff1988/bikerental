d568d5d74ea6428287275dba79ae9b428407a9a9
David Rüschhoff <drueschh@uni-muenster.de>
2024-02-23 10:09:54 +0100

    Klasseninhalt von User auf Reservation geändert

--- src/main/java/entity/reservation/Reservation.java
+++ src/main/java/entity/reservation/Reservation.java
@@ -1,59 +1,68 @@
 package entity.reservation;
 
+import java.time.LocalDateTime;
+
 public class Reservation {
 	
-	//Create parameters
-	protected String firstName;
-	protected String lastName;
-	protected String address;
-	protected String email;
-	protected String iban;
-	protected String bic;
-	protected String passwordhash;
+	//Startzeit erstellen
+	private LocalDateTime startTime;
+	//Buchungszeit erstellen
+	private LocalDateTime bookingTime;
+	//Endzeit erstellen
+	private LocalDateTime endTime;
+	//Preis der Reservierung erstellen
+	private int price;
+	
+	/*
+	 * Konstruktor für alle Parameter der Reservation
+	 * @param startTime Startzeit
+	 * @param bookingTime BuchungsZeit
+	 * @param endTime Endzeit
+	 * @param price Preis der Reservierung
+	 */
+	public Reservation(LocalDateTime startTime, LocalDateTime bookingTime, LocalDateTime endTime, int price) {
+		super();
+		this.startTime = startTime;
+		this.bookingTime = bookingTime;
+		this.endTime = endTime;
+		this.price = price;
+	}
+	
+	//Startzeit zurückgeben
+	public LocalDateTime getStartTime() {
+		return startTime;
+	}
+	//Startzeit setzten
+	public void setStartTime(LocalDateTime startTime) {
+		this.startTime = startTime;
+	}
+	//BuchungsZeit zurückgeben
+	public LocalDateTime getBookingTime() {
+		return bookingTime;
+	}
+	//BuchungsZeit setzten
+	public void setBookingTime(LocalDateTime bookingTime) {
+		this.bookingTime = bookingTime;
+	}
+	//Endzeit zurückgeben
+	public LocalDateTime getEndTime() {
+		return endTime;
+	}
+	//Endzeit setzten
+	public void setEndTime(LocalDateTime endTime) {
+		this.endTime = endTime;
+	}
+	//Preis der Reservierung zurückgeben
+	public int getPrice() {
+		return price;
+	}
+	//Preis der Reservierung setzten
+	public void setPrice(int price) {
+		this.price = price;
+	}
 	
 	
-	//Getter, Setter
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getIban() {
-		return iban;
-	}
-	public void setIban(String iban) {
-		this.iban = iban;
-	}
-	public String getBic() {
-		return bic;
-	}
-	public void setBic(String bic) {
-		this.bic = bic;
-	}
-	public String getPasswordhash() {
-		return passwordhash;
-	}
-	public void setPasswordhash(String passwordhash) {
-		this.passwordhash = passwordhash;
-	}
+	
+	
 	
 }
