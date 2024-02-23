e5ec3a1abb2a401c424f1a79b75e2b1c03192af7
Nisa Akcocuk <n_akco01@uni-muenster.de>
2024-02-23 16:38:01 +0100

    klassen und ordnerstruktur erstellt

---
+++ src/main/java/de/wwu/sopra/useradministration/addUserGUI.java
@@ -0,0 +1,23 @@
+package de.wwu.sopra.useradministration;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class addUserGUI extends Application {
+	
+
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
+	
+
+	
+	
+}
