4e6f16b63627b9d1ae232c65aaa582f2e6aa6514
Auridh <auridh@auridh.me>
2024-02-25 00:00:13 +0100

    :bug: Fixe falschen lambda Aufruf

--- src/main/java/de/wwu/sopra/App.java
+++ src/main/java/de/wwu/sopra/App.java
@@ -40,7 +40,7 @@ public class App extends Application {
 		userManagementGUI.onLogin(user -> {
 			var mainGUI = MainGUI.init(user);
 			scene.setRoot(mainGUI);
-			mainGUI.onLogout(unused -> scene.setRoot(userManagementGUI));
+			mainGUI.onLogout(() -> scene.setRoot(userManagementGUI));
 		});
 
 		primaryStage.setFullScreen(true);
