b4222b6af69d01416e6f47aa16f56741401c5971
Auridh <auridh@auridh.me>
2024-02-25 00:17:23 +0100

    :bug: Mache das Passwort-Feld zu einem PasswordField (vorher TextField)

--- src/main/java/de/wwu/sopra/login/LoginGUI.java
+++ src/main/java/de/wwu/sopra/login/LoginGUI.java
@@ -3,6 +3,7 @@ package de.wwu.sopra.login;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
 
@@ -34,7 +35,7 @@ public class LoginGUI extends HBox {
         emailInput.setPromptText("E-Mail");
         emailInput.setPrefWidth(400);
 
-        var passwordInput = new TextField();
+        var passwordInput = new PasswordField();
         passwordInput.setPromptText("Passwort");
         passwordInput.setPrefWidth(400);
 
