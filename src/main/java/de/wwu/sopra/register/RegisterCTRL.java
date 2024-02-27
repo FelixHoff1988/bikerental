0c95afab7763c69b73d4faccaf9b71d253473669
Ben Schnitzler <bschnitz@uni-muenster.de>
2024-02-27 12:29:26 +0100

    added saving to registration, finalized GUI

--- src/main/java/de/wwu/sopra/register/RegisterCTRL.java
+++ src/main/java/de/wwu/sopra/register/RegisterCTRL.java
@@ -1,8 +1,14 @@
 package de.wwu.sopra.register;
 
+import java.util.ArrayList;
 import java.util.regex.Pattern;
 
+import de.wwu.sopra.DataProvider;
+import de.wwu.sopra.PasswordHashing;
 import de.wwu.sopra.UserManagementGUI;
+import de.wwu.sopra.entity.Reservation;
+import de.wwu.sopra.entity.User;
+import de.wwu.sopra.entity.UserRole;
 import javafx.scene.control.TextField;
 
 public class RegisterCTRL {
@@ -10,8 +16,25 @@ public class RegisterCTRL {
 		
 	}
 	
-	public void registerUser() {
-		
+	public void registerUser(TextField[] textFieldsRegistration) {
+		for ( TextField element : textFieldsRegistration)
+		{
+			System.out.println(element.getText());
+		}
+		User registeredUser = new User(
+			textFieldsRegistration[0].getText(),
+			textFieldsRegistration[1].getText(),
+			textFieldsRegistration[2].getText() + " " +  textFieldsRegistration[3].getText() + " " + textFieldsRegistration[4].getText() + " " + textFieldsRegistration[5].getText(),
+			textFieldsRegistration[8].getText(),
+			textFieldsRegistration[6].getText(),
+			textFieldsRegistration[7].getText(),
+			PasswordHashing.hashPassword(textFieldsRegistration[9].getText()),
+			UserRole.CUSTOMER
+		);
+
+		DataProvider prov = DataProvider.getInstance();
+		prov.addUser(registeredUser);
+		UserManagementGUI.getInstance().login(registeredUser);
 	}
 	
 	public boolean testTextField(String regex, TextField textField)
