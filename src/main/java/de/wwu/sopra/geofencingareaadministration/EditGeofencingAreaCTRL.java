363b406f41cdae7d981e0d489f6feae6f74f907a
eike.wolff <eike.wolff@uni-muenster.de>
2024-03-04 14:55:15 +0100

    :art: :memo: Kommentare & Imports

--- src/main/java/de/wwu/sopra/geofencingareaadministration/EditGeofencingAreaCTRL.java
+++ src/main/java/de/wwu/sopra/geofencingareaadministration/EditGeofencingAreaCTRL.java
@@ -1,31 +1,19 @@
 package de.wwu.sopra.geofencingareaadministration;
 
-import java.util.List;
-import java.util.regex.Pattern;
-
-import com.sothawo.mapjfx.Coordinate;
-
 import de.wwu.sopra.AppContext;
 import de.wwu.sopra.DataProvider;
-import de.wwu.sopra.entity.BikeStation;
 import de.wwu.sopra.entity.GeofencingArea;
 import de.wwu.sopra.map.MapGUI;
-import javafx.scene.control.TextField;
 
+/**
+ * Steuerungsklasse zur GeofencingArea GUI Klasse
+ */
 public class EditGeofencingAreaCTRL {
     /**
      * Constructor
      */
     public EditGeofencingAreaCTRL() {
 
-    }
-
-    /**
-     * lädt die liste aller GeofencingAreas
-     * @return Liste aller GeofencingAreas
-     */
-    public List<GeofencingArea> loadGeofencingAreas() {
-        return DataProvider.getInstance().getGeoAreas();
     }
     
     /**
