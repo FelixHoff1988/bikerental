b670f2c30b0edb3500239b60b78bf7447edde6b4
eike.wolff <eike.wolff@uni-muenster.de>
2024-03-04 13:27:35 +0100

    Booking Process Klasse

---
+++ src/main/java/de/wwu/sopra/bookingProcess/BookingProcessService.java
@@ -0,0 +1,11 @@
+package de.wwu.sopra.bookingProcess;
+
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+
+public class BookingProcessService extends Service<Void> {
+    @Override
+    protected Task<Void> createTask() {
+        return null;
+    }
+}
