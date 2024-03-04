f37fd2a1be63277aaa33d5e74b3ae798a2b46010
eike.wolff <eike.wolff@uni-muenster.de>
2024-03-04 17:01:57 +0100

    :construction: Service Test-Implementation

--- src/main/java/de/wwu/sopra/bookingProcess/BookingProcessService.java
+++ src/main/java/de/wwu/sopra/bookingProcess/BookingProcessService.java
@@ -1,11 +1,34 @@
 package de.wwu.sopra.bookingProcess;
 
+import de.wwu.sopra.DataProvider;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 public class BookingProcessService extends Service<Void> {
     @Override
     protected Task<Void> createTask() {
-        return null;
+        var service = this;
+        return new Task<>() {
+            @Override
+            protected Void call() throws Exception {
+                DataProvider
+                        .getInstance()
+                        .getReservations()
+                        .stream()
+                        .filter(res -> res.getEndTime() == null
+                                && res.getBookingTime() == null
+                                && res.getStartTime().until(LocalDateTime.now(), ChronoUnit.MILLIS) >= 60000)
+                        .forEach(res -> res.setEndTime(LocalDateTime.now()));
+
+                wait(60000);
+
+                service.reset();
+                service.start();
+                return null;
+            }
+        };
     }
 }
