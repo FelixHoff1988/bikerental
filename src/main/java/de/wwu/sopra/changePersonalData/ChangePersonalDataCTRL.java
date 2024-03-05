b65632f68c2d0b89268858ddf24aabdf17132191
Eike Elias Yondalar Gwiasda <egwiasda@uni-muenster.de>
2024-03-05 12:35:14 +0100

    Funktionalität fertiggestellt

--- src/main/java/de/wwu/sopra/changePersonalData/ChangePersonalDataCTRL.java
+++ src/main/java/de/wwu/sopra/changePersonalData/ChangePersonalDataCTRL.java
@@ -1,6 +1,12 @@
 package de.wwu.sopra.changePersonalData;
 
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+import de.wwu.sopra.AppContext;
+import de.wwu.sopra.PasswordHashing;
 import de.wwu.sopra.entity.User;
+import javafx.scene.control.TextField;
 
 /**
  * Kontrollklasse um eigene Benutzerdaten einzusehen und zu ändern
@@ -8,10 +14,48 @@ import de.wwu.sopra.entity.User;
 public class ChangePersonalDataCTRL {
 
     /**
+     * Instanz des AppContext
+     */
+    private AppContext context = AppContext.getInstance();
+
+    /**
      * Standartkonstruktor
      */
     public ChangePersonalDataCTRL() {
 
+    }
+
+    /**
+     * Wird bei korrekten Werten für Textfields aufgerufen, speichert angegebene
+     * Daten
+     * 
+     * @param textFieldsRegistration
+     * @param
+     */
+    public void changeData(TextField[] textFieldsRegistration, boolean newPassword) {
+        var user = context.getLoggedInUser();
+
+        user.setFirstName(textFieldsRegistration[0].getText());
+
+        user.setLastName(textFieldsRegistration[1].getText());
+
+        user.setStreet(textFieldsRegistration[2].getText());
+
+        user.setHouseNumber((int) Integer.valueOf(textFieldsRegistration[3].getText()));
+
+        user.setPostalCode((int) Integer.valueOf(textFieldsRegistration[4].getText()));
+
+        user.setCity(textFieldsRegistration[5].getText());
+
+        user.setEmail(textFieldsRegistration[8].getText());
+
+        user.setIban(textFieldsRegistration[6].getText());
+
+        user.setBic(textFieldsRegistration[7].getText());
+
+        if (newPassword) {
+            user.setPasswordHash(PasswordHashing.hashPassword(textFieldsRegistration[9].getText()));
+        }
     }
 
     /**
@@ -42,4 +86,21 @@ public class ChangePersonalDataCTRL {
         return role;
     }
 
+    /**
+     * Prüft, ob Eingabe in TextField von Registration korrekt ist
+     *
+     * @param regex     Regex code um Korrektheit der Eingabe eines Feldes zu
+     *                  überprüfen
+     * @param textField Text Field aus Registrierungsform
+     * @return Gibt zurück, ob das Textfeld korrekt gesetzt wurde
+     */
+    public boolean testTextField(String regex, TextField textField) {
+        if (!Pattern.matches(regex, textField.getText())) {
+            textField.setStyle("-fx-background-color: #FFA59D;");
+            return false;
+        } else {
+            textField.setStyle("");
+            return true;
+        }
+    }
 }
--- src/main/java/de/wwu/sopra/changePersonalData/ChangePersonalDataGUI.java
+++ src/main/java/de/wwu/sopra/changePersonalData/ChangePersonalDataGUI.java
@@ -26,11 +26,7 @@ import javafx.scene.layout.VBox;
  * GUI für die Bearbeitung eigener Nutzerdaten
  */
 public class ChangePersonalDataGUI extends HBox {
-    
-    /**
-     * Instanz des DataProviders
-     */
-    private DataProvider data = DataProvider.getInstance();
+
     /**
      * Die Kontrollklasse um Aktionen auszuführen
      */
@@ -39,7 +35,7 @@ public class ChangePersonalDataGUI extends HBox {
      * Instanz des AppContext
      */
     private AppContext context = AppContext.getInstance();
-    
+
     /**
      * Standartkonstruktor
      */
@@ -47,7 +43,7 @@ public class ChangePersonalDataGUI extends HBox {
         var user = context.getLoggedInUser();
         init(user);
     }
-    
+
     /**
      * Initialisiert die GUI-Übersicht zur bearbeitung der eigenen Nutzerdaten
      * 
@@ -59,132 +55,153 @@ public class ChangePersonalDataGUI extends HBox {
         innerBox.setPadding(new Insets(25, 25, 25, 25));
         innerBox.setAlignment(Pos.CENTER);
         innerBox.setVgap(5);
-        
+
         var firstName = user.getFirstName();
         var firstNameLabel = new Label("Vorname: ");
         var firstNameTextField = new TextField(firstName);
         innerBox.add(firstNameLabel, 0, 0);
         innerBox.add(firstNameTextField, 1, 0);
-        
+
         var lastName = user.getLastName();
         var lastNameLabel = new Label("Nachname: ");
         var lastNameTextField = new TextField(lastName);
         innerBox.add(lastNameLabel, 0, 1);
         innerBox.add(lastNameTextField, 1, 1);
-        
+
         var role = ctrl.getRoleString(user);
         var roleNameLabel = new Label("Rolle: ");
         var roleLabel = new Label(role);
         innerBox.add(roleNameLabel, 0, 2);
         innerBox.add(roleLabel, 1, 2);
-        
+
         var spacer1 = new Pane();
         innerBox.add(spacer1, 0, 3);
         var spacer2 = new Pane();
         innerBox.add(spacer2, 0, 4);
-        
-        
+
         var street = user.getStreet();
         var streetLabel = new Label("Stra\u00DFe: ");
         var streetTextField = new TextField(street);
         innerBox.add(streetLabel, 0, 4);
         innerBox.add(streetTextField, 1, 4);
-        
+
         var houseNumber = String.valueOf(user.getHouseNumber());
         var houseNumberLabel = new Label("Hausnummer: ");
         var houseNumberTextField = new TextField(houseNumber);
         innerBox.add(houseNumberLabel, 0, 5);
         innerBox.add(houseNumberTextField, 1, 5);
-        
+
         var plz = String.valueOf(user.getPostalCode());
         var plzLabel = new Label("PLZ: ");
         var plzTextField = new TextField(plz);
         innerBox.add(plzLabel, 0, 6);
         innerBox.add(plzTextField, 1, 6);
-        
+
         var town = user.getCity();
         var townLabel = new Label("Stadt: ");
         var townTextField = new TextField(town);
         innerBox.add(townLabel, 0, 7);
         innerBox.add(townTextField, 1, 7);
-        
+
         var iban = String.valueOf(user.getIban());
-        var IBANLabel = new Label("IBAN: ");
-        var IBANTextField = new TextField(iban);
-        innerBox.add(IBANLabel, 0, 8);
-        innerBox.add(IBANTextField, 1, 8);
-        
+        var ibanLabel = new Label("IBAN: ");
+        var ibanTextField = new TextField(iban);
+        innerBox.add(ibanLabel, 0, 8);
+        innerBox.add(ibanTextField, 1, 8);
+
         var bic = user.getBic();
-        var BICLabel = new Label("BIC: ");
-        var BICTextField = new TextField(bic);
-        innerBox.add(BICLabel, 0, 9);
-        innerBox.add(BICTextField, 1, 9);
-        
+        var bicLabel = new Label("BIC: ");
+        var bicTextField = new TextField(bic);
+        innerBox.add(bicLabel, 0, 9);
+        innerBox.add(bicTextField, 1, 9);
+
         var email = user.getEmail();
         var emailLabel = new Label("E-Mail: ");
         var emailTextField = new TextField(email);
         innerBox.add(emailLabel, 4, 0);
         innerBox.add(emailTextField, 5, 0);
-        
-        var PasswordLabel = new Label("Neues Passwort: ");
-        var PasswordTextField = new PasswordField();
-        innerBox.add(PasswordLabel, 4, 1);
-        innerBox.add(PasswordTextField, 5, 1);
-        
-        var VerPasswordLabel = new Label("Passwort bestätigen: ");
-        var VerPasswordTextField = new PasswordField();
-        innerBox.add(VerPasswordLabel, 4, 2);
-        innerBox.add(VerPasswordTextField, 5, 2);
-        
+
+        var passwordLabel = new Label("Neues Passwort: ");
+        var passwordTextField = new PasswordField();
+        innerBox.add(passwordLabel, 4, 1);
+        innerBox.add(passwordTextField, 5, 1);
+
+        var verPasswordLabel = new Label("Passwort bestätigen: ");
+        var verPasswordTextField = new PasswordField();
+        innerBox.add(verPasswordLabel, 4, 2);
+        innerBox.add(verPasswordTextField, 5, 2);
+
         // Button zum Speichern
         var submitButton = new Button("Speichern");
         innerBox.add(submitButton, 0, 10);
-        
-        
-        var passwordRequirements = new Label(
-                "Paswörter stimmen überein.\r\n"
-                + "Paswort beinhaltet:\r\n"
-                + "- eine Nummer (0-9)\r\n"
-                + "- einen großen Buchstaben\r\n"
-                + "- einen kleinen Buchstaben\r\n"
-                + "- ein Sonderzeichen\r\n"
-                + "- 6-18 Zeichen, ohne Leerzeichen");
+
+        var passwordRequirements = new Label("Paswörter stimmen überein.\r\n" + "Paswort beinhaltet:\r\n"
+                + "- eine Nummer (0-9)\r\n" + "- einen großen Buchstaben\r\n" + "- einen kleinen Buchstaben\r\n"
+                + "- ein Sonderzeichen\r\n" + "- 6-18 Zeichen, ohne Leerzeichen");
         innerBox.add(passwordRequirements, 5, 3);
-        
-//        submitButton.setOnAction(event -> {
-//            
-//            ArrayList<Boolean> list = new ArrayList<Boolean>();
-//            
-//            list.add(ctrl.testTextField("^[\\p{L} ,.'-]+$", firstNameTextField));
-//            list.add(ctrl.testTextField("^[\\p{L} ,.'-]+$", lastNameTextField));
-//            list.add(ctrl.testTextField("^[\\p{L} ,.'-]+$", streetTextField));
-//            list.add(ctrl.testTextField("-?\\d+\\.?\\d*", houseNumberTextField));
-//            list.add(ctrl.testTextField("-?\\d+\\.?\\d*", plzTextField));
-//            list.add(ctrl.testTextField("^[\\p{L} ,.'-]+$", townTextField));
-//            list.add(ctrl.testTextField("^DE[0-9]{20}$", IBANTextField));
-//            list.add(ctrl.testTextField("([a-zA-Z]{4})([a-zA-Z]{2})(([2-9a-zA-Z]{1})([0-9a-np-zA-NP-Z]{1}))((([0-9a-wy-zA-WY-Z]{1})([0-9a-zA-Z]{2}))|([xX]{3})|)", BICTextField));
-//            list.add(ctrl.testTextField("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", emailTextField));
-//            list.add(ctrl.testTextField("^(?=.*\\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\\w\\d\\s:])([^\\s]){8,16}$", PasswordTextField));
-//            list.add(ctrl.testTextField("^(?=.*\\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\\w\\d\\s:])([^\\s]){8,16}$", VerPasswordTextField));
-//            
-//            Boolean passwordsEqual = (PasswordTextField.getText().equals(VerPasswordTextField.getText()));
-//            if (!passwordsEqual)
-//            {
-//                VerPasswordTextField.setStyle("-fx-background-color: #FFA59D;");
-//            }
-//            
-//            list.add(passwordsEqual);
-//            
-//            if (areAllTrue(list))
-//            {
-//                TextField[] textFieldsRegistration = innerBox.getChildren().stream().filter(node -> node.getClass() == TextField.class || node.getClass() == PasswordField.class).toArray(TextField[]::new);
-//                ctrl.registerUser(textFieldsRegistration);
-//            }
-//            
-//        });
-        
+
+        submitButton.setOnAction(event -> {
+
+            ArrayList<Boolean> list = new ArrayList<Boolean>();
+
+            list.add(ctrl.testTextField("^[\\p{L} ,.'-]+$", firstNameTextField));
+            list.add(ctrl.testTextField("^[\\p{L} ,.'-]+$", lastNameTextField));
+            list.add(ctrl.testTextField("^[\\p{L} ,.'-]+$", streetTextField));
+            list.add(ctrl.testTextField("-?\\d+\\.?\\d*", houseNumberTextField));
+            list.add(ctrl.testTextField("-?\\d+\\.?\\d*", plzTextField));
+            list.add(ctrl.testTextField("^[\\p{L} ,.'-]+$", townTextField));
+            list.add(ctrl.testTextField("^DE[0-9]{20}$", ibanTextField));
+            list.add(ctrl.testTextField(
+                    "([a-zA-Z]{4})([a-zA-Z]{2})(([2-9a-zA-Z]{1})([0-9a-np-zA-NP-Z]{1}))((([0-9a-wy-zA-WY-Z]{1})([0-9a-zA-Z]{2}))|([xX]{3})|)",
+                    bicTextField));
+            list.add(ctrl.testTextField("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", emailTextField));
+
+            var newPassword = false;
+            if (!passwordTextField.getText().isEmpty()) {
+                newPassword = true;
+                
+                list.add(ctrl.testTextField("^(?=.*\\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\\w\\d\\s:])([^\\s]){8,16}$",
+                        passwordTextField));
+                list.add(ctrl.testTextField("^(?=.*\\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\\w\\d\\s:])([^\\s]){8,16}$",
+                        verPasswordTextField));
+                
+                Boolean passwordsEqual = (passwordTextField.getText().equals(verPasswordTextField.getText()));
+                if (!passwordsEqual) {
+                    verPasswordTextField.setStyle("-fx-background-color: #FFA59D;");
+                }
+
+                list.add(passwordsEqual);
+            }
+
+            if (areAllTrue(list)) {
+                TextField[] textFieldsRegistration = innerBox.getChildren().stream()
+                        .filter(node -> node.getClass() == TextField.class || node.getClass() == PasswordField.class)
+                        .toArray(TextField[]::new);
+                ctrl.changeData(textFieldsRegistration, newPassword);
+                
+                AppContext.getInstance().showMessage(
+                        "Daten erfolgreich geändert.",
+                        5,
+                        "#FFCCDD");
+            }
+
+        });
+
         this.getChildren().addAll(innerBox);
         this.setAlignment(Pos.CENTER);
         VBox.setVgrow(this, Priority.ALWAYS);
+    }
+
+    /**
+     * Prüft, ob alle Elemente einer ArrayList von Typ Boolean wahr sind
+     * 
+     * @param arrayList
+     * @return
+     */
+    private static boolean areAllTrue(ArrayList<Boolean> arrayList) {
+        for (boolean b : arrayList)
+            if (!b)
+                return false;
+        return true;
     }
 }
